9a1d3db9baec41a2f25965c8db9368e5f17fa9b6
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2025-11-04 12:42:47 +0100

    Zadanie3

---
+++ Zadania/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Zadania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaCelsjusz = 25;
+            double farenheit = (temperaturaCelsjusz * 9 / 5) + 32;
+            temperaturaCelsjusz = (farenheit - 32) * 5 / 9;
+            Console.WriteLine($"Temperatura w Farenheitach: {Math.Round(farenheit, 2)}");
+            Console.WriteLine($"Konwersja z powrotem do Celsjusza: {Math.Round(temperaturaCelsjusz, 2)}");
+            if (temperaturaCelsjusz < 0)
+            {
+                Console.WriteLine("Uwazaj temperatura poniżej zera!");
+            }
+        }
+    }
+}
