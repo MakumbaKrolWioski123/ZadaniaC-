e137858f787cbc0915048498ced4d590154aef65
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2025-11-07 13:06:00 +0100

    Klasa z konstruktorem

---
+++ Klasa_konstruktory/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Klasa_enkapsulacja
+{
+    public class Product
+    {
+        private string nazwa;
+        private decimal cena;
+        private int iloscNaMagazynie;
+
+        public string Nazwa
+        {
+            get { return nazwa; }
+            set { nazwa = value; }
+        }
+
+        public decimal Cena
+        {
+            get { return cena; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Cena nie może być ujemna.");
+                }
+                cena = value;
+            }
+        }
+        public int IloscNaMagazynie
+        {
+            get { return iloscNaMagazynie; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ilość na magazynie nie może być ujemna.");
+                }
+                iloscNaMagazynie = value;
+            }
+        }
+
+        public Product(string nazwa, decimal cena, int iloscNaMagazynie)
+        {
+            this.Nazwa = nazwa;
+            this.Cena = cena;
+            this.IloscNaMagazynie = iloscNaMagazynie;
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Nazwa produktu: {Nazwa}");
+            Console.WriteLine($"Cena: {Cena} PLN");
+            Console.WriteLine($"Ilość na magazynie: {IloscNaMagazynie}");
+        }
+
+        public void ObliczCenePoRabacie(decimal procentRabatu)
+        {
+            if (procentRabatu < 0 || procentRabatu > 100)
+            {
+                throw new ArgumentException("Procent rabatu musi być między 0 a 100.");
+            }
+            decimal rabat = (procentRabatu / 100) * Cena;
+            decimal cenaPoRabacie = Cena - rabat;
+            Console.WriteLine($"Cena po rabacie {procentRabatu}%: {cenaPoRabacie} PLN");
+        }
+
+        public Boolean czyDostepny(int iloscNaMagazynie)
+        {
+            if (iloscNaMagazynie > 0)
+            {
+                Console.WriteLine("Produkt jest dostępny");
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine("Produkt jest niedostępny.");
+                return false;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var produkt = new Product("Laptop", -3500.99m, 10);
+            produkt.WyswietlInformacje();
+        }
+    }
+}
+
+
